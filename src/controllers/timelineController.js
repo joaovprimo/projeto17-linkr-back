6ada20f0554933e1f53e09117eb94676deaf5ca8
vinicius <vinicius.coassis@gmail.com>
2022-10-19 17:25:30 -0300

    feat: add publicate post && refactor: add url metadada info on post

--- src/controllers/timelineController.js
+++ src/controllers/timelineController.js
@@ -8,9 +8,22 @@ const postLink = async (req, res) => {
       'INSERT INTO posts(url, description, "userId") VALUES ($1,$2,$3)',
       [body.url, body.description, body.userId]
     );
+    const { canonical, image, title, description } = await urlMetadata(
+      body.url
+    );
+    await connection.query(
+      'INSERT INTO "urlInfo" (url, canonical, image, title, description) VALUES ($1,$2,$3,$4,$5)',
+      [body.url, canonical, image, title, description]
+    );
 
     return res.sendStatus(201);
   } catch (error) {
+    switch (error.code) {
+      case "23505":
+        return res.sendStatus(201);
+      case "ENOTFOUND":
+        return res.status(422).send("Link enviado está quebrado");
+    }
     return res.status(500).send(error);
   }
 };
@@ -19,16 +32,11 @@ const getTimeline = async (req, res) => {
   const allPosts = await connection.query("SELECT * FROM posts LIMIT 20;");
 
   for (let i = 0; i < allPosts.rows.length; i++) {
-    const { canonical, image, title, description } = await urlMetadata(
-      allPosts.rows[i].url
+    const urlInfo = await connection.query(
+      'SELECT canonical,image,title,description FROM "urlInfo" WHERE url = $1',
+      [allPosts.rows[i].url]
     );
-    let urlInfo = {
-      canonical,
-      image,
-      title,
-      description,
-    };
-    allPosts.rows[i].urlInfo = urlInfo;
+    allPosts.rows[i].urlInfo = urlInfo.rows[0];
   }
 
   return res.send(allPosts.rows);
