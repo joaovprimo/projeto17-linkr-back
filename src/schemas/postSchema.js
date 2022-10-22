1a8b6a1f0b471f1a142e5b4d97e371c77e64a209
vinicius <vinicius.coassis@gmail.com>
2022-10-22 10:55:37 -0300

    fix: allow null description

--- src/schemas/postSchema.js
+++ src/schemas/postSchema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const postSchema = Joi.object({
   url: Joi.string().required(),
-  description: Joi.string(),
+  description: Joi.string().allow(null, ''),
   userId: Joi.number().required(),
 });
 
