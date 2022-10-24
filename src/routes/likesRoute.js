0093d9929795589fb3e05f7efd6074e26e53254b
Rodrigo <rodrigovf2102@gmail.com>
2022-10-24 16:18:57 -0300

    preparing to merge

--- src/routes/likesRoute.js
+++ src/routes/likesRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 import {likesPost, getLikes, getUser} from "../controllers/likesController.js";
+import { validateToken } from "../middlewares/validateToken.js";
 
 const likesRoute = express.Router();
 
-likesRoute.get(`/posts/likes/:id`, getLikes)
-likesRoute.post(`/posts/likes/:id/:idusr`,likesPost)
+likesRoute.get(`/posts/likes/:id`, validateToken,getLikes)
+likesRoute.post(`/posts/likes/:id/:idusr`,validateToken,likesPost)
 likesRoute.get(`/user/:id`, getUser)
 
 
