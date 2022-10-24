5711a1876dc1889ab1783aca144cd46eeb9bb5e5
JOAO VITOR PRIMO DE OLIVEIRA <jv.primo26@gmail.com>
2022-10-24 15:16:46 -0300

    likes edited ok

--- src/routes/likesRoute.js
+++ src/routes/likesRoute.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { validateToken } from "../middlewares/validateToken.js"
 import {likesPost, getLikes, getUser} from "../controllers/likesController.js";
 
 const likesRoute = express.Router();
 
 likesRoute.get(`/posts/likes/:id`, getLikes)
-likesRoute.post(`/posts/likes/:id/:idusr`,validateToken,likesPost)
+likesRoute.post(`/posts/likes/:id/:idusr`,likesPost)
 likesRoute.get(`/user/:id`, getUser)
 
 
