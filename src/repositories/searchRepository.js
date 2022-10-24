5967792079b206886ed78617707bbdd6791ba102
Driven Education <yessica_stern@hotmail.com>
2022-10-24 02:26:29 -0300

    funções de busca melhoradas

--- src/repositories/searchRepository.js
+++ src/repositories/searchRepository.js
@@ -2,7 +2,11 @@ import connection from "../database/database.js"
 
 async function searchUsersRepository (lim, search){
         const response = (await connection.query(`SELECT * FROM users WHERE username ILIKE $1 ${lim}`,[`%${search}%`])).rows;
-        return response
+        return response;
 }
 
-export {searchUsersRepository};
+async function getNameByIdRepository (id){
+        return (await connection.query(`SELECT * FROM users WHERE id=$1`,[id])).rows;
+}
+
+export {searchUsersRepository,getNameByIdRepository};
