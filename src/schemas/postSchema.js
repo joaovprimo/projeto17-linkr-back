86a06cf0f5ae5ba74306e98c65e043b8d9db6381
vinicius <vinicius.coassis@gmail.com>
2022-10-25 11:52:01 -0300

    feat: add reposter possibility

--- src/schemas/postSchema.js
+++ src/schemas/postSchema.js
@@ -4,6 +4,7 @@ const postSchema = Joi.object({
   url: Joi.string().required(),
   description: Joi.string().allow(null, ''),
   userId: Joi.number().required(),
+  reposterId: Joi.number().allow(null, ''),
 });
 
 export default postSchema;
