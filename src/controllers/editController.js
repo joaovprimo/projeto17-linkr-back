61a356518d4d1498c5a5e0de867d2d9c26b41c77
Rodrigo <rodrigovf2102@gmail.com>
2022-10-28 00:09:24 -0300

    feat: posts add, delete and update improved v2

--- src/controllers/editController.js
+++ src/controllers/editController.js
@@ -1,43 +1,45 @@
+import { StatusCodes } from "http-status-codes";
 import { deleteTrendsByDescription, deletPostTrendsByPostId } from "../repositories/deleteRepository.js";
-import { getPostById, updatePost} from "../repositories/editRepository.js";
+import { getPostById, updatePost } from "../repositories/editRepository.js";
 import { getPostByTrend } from "../repositories/postRepository.js";
 import { getTrendbyName, insertNewTrend, insertPostTrends } from "../repositories/trendsRepository.js";
 
-export async function editPost (req, res){
+export async function editPost(req, res) {
     const user = res.locals.user;
-    const {id} = req.params;
-    const {description} = req.body;
-    
-    try{
+    const { id } = req.params;
+    const { description } = req.body;
+
+    try {
         const postUser = (await getPostById(id)).rows[0];
-        if(user.id !== postUser.userId){
-            return res.status(401).send("Action not allowed, the user does not match with the post's owner");
+        if (user.id !== postUser.userId) {
+            return res.status(StatusCodes.UNAUTHORIZED)
+            .send("Action not allowed, the user does not match with the post's owner");
         }
 
         await deletPostTrendsByPostId(id);
         const OldTrendsArray = postUser.description.split('#');
-        for(let i=1;i<=OldTrendsArray.length-1;i++) {
-            const trends = await getPostByTrend('#'+OldTrendsArray[i].trim());
-            if(trends.rowCount===1){
+        for (let i = 1; i <= OldTrendsArray.length - 1; i++) {
+            const trends = await getPostByTrend('#' + OldTrendsArray[i].trim());
+            if (trends.rowCount === 1) {
                 await deleteTrendsByDescription(OldTrendsArray[i].trim());
             }
         }
 
         const NewTrendsArray = description.split('#');
-        for(let i=1;i<=NewTrendsArray.length-1;i++) {
+        for (let i = 1; i <= NewTrendsArray.length - 1; i++) {
             let trends = await getTrendbyName(NewTrendsArray[i].trim())
-            if(trends.rowCount === 0){
+            if (trends.rowCount === 0) {
                 await insertNewTrend(NewTrendsArray[i].trim());
                 trends = (await getTrendbyName(NewTrendsArray[i].trim())).rows[0];
             }
-            await insertPostTrends(id,trends.id)
+            await insertPostTrends(id, trends.id)
         }
-        
-        await updatePost(description,id,user.id);
+
+        await updatePost(description, id, user.id);
         const postEdited = await getPostById(id);
-        return res.status(200).send(postEdited.rows[0].description)
-    }catch(err){
+        return res.status(StatusCodes.OK).send(postEdited.rows[0].description)
+    } catch (err) {
         console.log(err.message)
-        return res.status(500).send(err.message);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
     }
 }
