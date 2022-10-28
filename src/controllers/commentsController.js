b3a23a4dbddebdbfe34989b7e49d2bd7ecb0ab4f
JouJou <joujou@pop-os.localdomain>
2022-10-28 15:36:50 -0300

    ENding back comments

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -1,13 +1,15 @@
 import { response } from "express";
 import connection from "../database/database.js";
+import {
+  getCommentsById,
+  insertComment,
+} from "../repositories/commentsRepository.js";
 
 export async function postComment(req, res) {
   const { id } = res.locals.user;
+  console.log(id);
   try {
-    await connection.query(
-      `INSERT INTO comments ("userId", "postId", comment) VALUES ($1, $2, $3)`,
-      [id, req.body.postId, req.body.comment]
-    );
+    await insertComment(id, req.body.postId, req.body.comment);
     res.sendStatus(200);
   } catch (err) {
     console.log(err);
@@ -18,13 +20,7 @@ export async function postComment(req, res) {
 export async function getComments(req, res) {
   try {
     const { id } = req.params;
-    const { rows: comments } = await connection.query(
-      `SELECT comments.*, users.username, users."pictureUrl"
-FROM comments
-JOIN users ON comments."userId" = users.id
-WHERE "postId" = $1`,
-      [id]
-    );
+    const { rows: comments } = await getCommentsById(id);
     return res.send(comments);
   } catch {
     return res.sendStatus(500);
